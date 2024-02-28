b97c1bf76f38b9870da549166c50a929d96eff71
Mathias Jensen <matje@space.dtu.dk>
2024-02-28 09:58:58 +0100

    Phase discrepancy investigation Beaufort

--- src/filehandling/cryosat_combine.py
+++ src/filehandling/cryosat_combine.py
@@ -2,42 +2,70 @@ import xarray as xr
 from pathlib import Path
 from tqdm import tqdm
 
+PHASE = 1
+lat_min, lat_max, lon_min, lon_max = [70, 85, -180, -120]
+
 # Select variables of interest
 variables = [
     'sea_level_anomaly',
     'sea_level_anomaly_filtered',
-    'sea_level_anomaly_uncertainty',
     'sea_level_anomaly_raw'
 ]
 
 # folder name
-folder = "phase21_arctic_all_years" 
+folder = f"phase{PHASE}_arctic_all_years" 
 # Path to data folder and files
-data_folder = Path(f"data/{folder}")
+data_folder = Path(f"D:/CryoTempo files/{folder}")
 # Make save folder
-save_folder_years = Path(data_folder, "save_folder_years")
+save_folder_years = Path("data", f"save_folder_Beaufort_phase{PHASE}")
 save_folder_years.mkdir(parents=True, exist_ok=True)
 
 
-years = list(range(10,22))
-
-for year in tqdm(years, position=0):
-    if Path(save_folder_years, f"combined_{folder}_20{year}.nc").exists():
+for folder in data_folder.glob(f"20[1-2][0-9]"):
+    if Path(save_folder_years, f"combined_{folder.stem}.nc").exists():
         continue
-    files = list(data_folder.glob(f"combined_{folder}_20{year}*.nc"))
-    initial_data = xr.open_dataset(files[0])
-    combined_data = [initial_data[variables]]
-    
-    for file in files[1:]:
-        # Consequtively subset each remaining day and add to data set
-        temporary_data = xr.open_dataset(file)
-        combined_data.append(temporary_data[variables])
+    combined_data = []
+    for file in tqdm(list(folder.rglob("*.nc"))):
+        tmp_data = xr.open_dataset(file)
+        beaufort_tmp_data = tmp_data.isel(
+            time=(
+                    tmp_data.latitude > lat_min
+                ) & (
+                    tmp_data.latitude < lat_max
+                ) & (
+                    tmp_data.longitude > lon_min
+                ) & (
+                    tmp_data.longitude < lon_max
+                )
+            )
+
+        if len(beaufort_tmp_data.time) > 0:
+            combined_data.append(beaufort_tmp_data[variables])
 
     combined_data = xr.concat(combined_data, dim="time")
 
-        # Sort by time
+    # Sort by time
     combined_data = combined_data.sortby("time")
 
-        # Save to file
-    combined_data.to_netcdf(Path(save_folder_years, f"combined_{folder}_20{year}.nc"))
+    # Save to file
+    combined_data.to_netcdf(Path(save_folder_years, f"combined_{folder.stem}.nc"))
+# for year in tqdm(years, position=0):
+#     if Path(save_folder_years, f"combined_{folder}_20{year}.nc").exists():
+#         continue
+#     files = list(data_folder.glob(f"combined_{folder}_20{year}*.nc"))
+#     initial_data = xr.open_dataset(files[0])
+#     combined_data = [initial_data[variables]]
+    
+#     for file in files[1:]:
+#         # Consequtively subset each remaining day and add to data set
+#         temporary_data = xr.open_dataset(file)
+#         combined_data.append(temporary_data[variables])
+
+#     combined_data = xr.concat(combined_data, dim="time")
+
+#         # Sort by time
+#     combined_data = combined_data.sortby("time")
+
+#         # Save to file
+#     combined_data.to_netcdf(Path(save_folder_years, f"combined_{folder}_20{year}.nc"))
 
--- yearly_average_arctic.py
+++ yearly_average_arctic.py
@@ -3,39 +3,44 @@ from pathlib import Path
 from tqdm import tqdm
 import numpy as np
 
-PHASE = 21 # 1 or 21
-MONTH = 8
-BASEPATH = Path(f"D:/CryoTempo files/phase{PHASE}_arctic_all_years")
-SAVEPATH = Path(f"data/yearly_average_phase{PHASE}")
-
+PHASE = 1 # 1 or 21
+MONTHS = [1, 2, 3, 6, 7, 8]
+BASEPATH = Path(f"data/save_folder_Beaufort_phase{PHASE}")
+SAVEPATH = Path(f"data/yearly_average_Beaufort_phase{PHASE}")
+SAVEPATH.mkdir(parents=True, exist_ok=True)
 
 def main():
-    SAVEPATH.mkdir(parents=True, exist_ok=True)
-    years = range(2011, 2024)
-    average_sla, average_slar, average_slaf = [], [], []
-    for year in tqdm(years, position=0):
-        files = list(Path(BASEPATH).glob(f"combined_phase{PHASE}_arctic_all_years_{year}_{MONTH:02d}*.nc"))
-        sla, slaf, slar = [],[],[]
-        for file in tqdm(files, position=1):
-            try:
-                temp_data = xr.open_dataset(file)
-            except ValueError:
-                continue
-            slar.append(np.nanmean(temp_data.sea_level_anomaly_raw))
-            sla.append(np.nanmean(temp_data.sea_level_anomaly))
-            slaf.append(np.nanmean(temp_data.sea_level_anomaly_filtered))
-        average_sla.append(np.nanmean(sla))
-        average_slar.append(np.nanmean(slar))
-        average_slaf.append(np.nanmean(slaf))
-    out_file = xr.Dataset(
-        data_vars={
-            "sea_level_anomaly_raw": average_slar,
-            "sea_level_anomaly": average_sla,
-            "sea_level_anomaly_filtered": average_slaf
-            },
-        coords={"time": [f"{year}-{MONTH:02d}" for year in years]}
-        )
-    out_file.to_netcdf(Path(SAVEPATH, f"yearly_averages_phase{PHASE}_{MONTH:02d}.nc"))
+    for month in MONTHS:
+        # if Path(SAVEPATH, f"yearly_averages_phase{PHASE}_{month:02d}.nc").exists():
+        #     continue
+        average_sla, average_slar, average_slaf, years = [], [], [], []
+        n_points = 0
+        for file in tqdm(list(BASEPATH.glob("*.nc"))):
+            data = xr.open_dataset(file)
+            year=file.stem[-4:]
+            years.append(year)
+            min_time = np.datetime64(f"{year}-{month:02d}-01")
+            max_time = np.datetime64(f"{year}-{month+1:02d}-01") # doesnt work with december
+            data_month = data.isel(time=(data.time < max_time) & (data.time >= min_time))
+            slar = np.nanmean(np.multiply(data_month.sea_level_anomaly_raw.values,np.cos(data_month.latitude.values*np.pi/180)))
+            sla = np.nanmean(np.multiply(data_month.sea_level_anomaly, np.cos(data_month.latitude*np.pi/180)))
+            slaf = np.nanmean(np.multiply(data_month.sea_level_anomaly_filtered, np.cos(data_month.latitude*np.pi/180)))
+
+            average_slar.append(slar)
+            average_sla.append(sla)
+            average_slaf.append(slaf)
+            n_points += len(data_month.time)
+
+        out_file = xr.Dataset(
+            data_vars={
+                "sea_level_anomaly_raw": average_slar,
+                "sea_level_anomaly": average_sla,
+                "sea_level_anomaly_filtered": average_slaf
+                },
+            coords={"time": [f"{yr}-{month:02d}" for yr in years]},
+            attrs={"N_points": n_points}
+            )
+        out_file.to_netcdf(Path(SAVEPATH, f"yearly_averages_phase{PHASE}_{month:02d}.nc"))
 
 if __name__ == "__main__":
     main()
